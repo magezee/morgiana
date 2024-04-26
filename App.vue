7158c442ea5297076058e7e6fa887cf894a53097
magezee <756591143@qq.com>
2024-04-26 15:30:49 +0800

    feat: 初始化

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">{{ demo }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const demo = ref(1222)
+
+</script>
+
+<style lang="less">
+.app {
+  background-color: red;
+}
+</style>
