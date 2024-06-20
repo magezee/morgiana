f1e7a3c7435bfd7e5b7bddf07b217e65a054f65d
magezee <756591143@qq.com>
2024-06-20 15:57:35 +0800

    feat: 测试webhook

--- server/api/webhook.ts
+++ server/api/webhook.ts
@@ -4,7 +4,7 @@ const router = Router()
 
 router.post('/webhook', async(req, res) => {
   console.log('Webhook received:', req.body);
-    // 在这里处理你的逻辑
+    // 在这里处理你的逻辑111
     res.status(200).send('Received!');
 })
 
